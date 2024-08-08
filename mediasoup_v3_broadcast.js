2559ba383dc7b927908b93b2254d09b08936abef
shane <shane@sidequestvr.com>
2024-08-08 23:15:30 +0200

    client side changes

--- mediasoup_v3_broadcast.js
+++ mediasoup_v3_broadcast.js
@@ -34,7 +34,7 @@
 const fs = require('fs');
 let serverOptions = {
   hostName: "screen.sdq.st",
-  listenPort: 443,
+  listenPort: 8443,
   useHttps: true,
   httpsKeyFile: "/usr/src/ssl/sdq.st.key",
   httpsCertFile: "/usr/src/ssl/sdq.st.cert"
